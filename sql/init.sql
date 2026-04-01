a69a2f7275bd3c203b3404203e18ba84b22ab939
BadSnail11 <BadSnail11@github.com>
2026-04-01 12:51:13 +0300

    init sql upd

--- sql/init.sql
+++ sql/init.sql
@@ -66,13 +66,13 @@ CREATE TABLE IF NOT EXISTS weekly_schedule (
 
 INSERT INTO weekly_schedule (weekday, day_name, is_open, open_time, close_time)
 VALUES
-    (0, 'monday', TRUE, '11:00', '22:00'),
-    (1, 'tuesday', TRUE, '11:00', '22:00'),
-    (2, 'wednesday', TRUE, '11:00', '22:00'),
-    (3, 'thursday', TRUE, '11:00', '22:00'),
-    (4, 'friday', TRUE, '11:00', '22:00'),
-    (5, 'saturday', TRUE, '11:00', '22:00'),
-    (6, 'sunday', TRUE, '11:00', '22:00')
+    (0, 'monday', TRUE, '16:00', '22:30'),
+    (1, 'tuesday', TRUE, '16:00', '22:30'),
+    (2, 'wednesday', TRUE, '16:00', '22:30'),
+    (3, 'thursday', TRUE, '16:00', '22:30'),
+    (4, 'friday', TRUE, '16:00', '23:30'),
+    (5, 'saturday', TRUE, '14:00', '23:30'),
+    (6, 'sunday', TRUE, '14:00', '22:30')
 ON CONFLICT (weekday) DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS telegram_recipients (
